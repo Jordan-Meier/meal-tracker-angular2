5d2b70511c2f12e098827752d587f02d5704d5dc
Jordan Meier <jbirdmeier@gmail.com>
2016-03-25 09:03:34 -0700

    add some hard coded meals to app component class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
 
 @Component({
   selector: 'my-app',
+  directives: [MealListComponent],
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>Today's Meals</h1>
+    <meal-list [mealList]="meals"></meal-list>
   </div>
     `
 })
 
 export class AppComponent {
   public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Odwalla Juice", "Strawberry C Monster smoothie blended juice", 300),
+      new Meal("Eggs and Sausage", "Need some of protein to start the day", 285),
+      new Meal("Snack Bar", "A healthy little mid morning snack", 120),
+      new Meal("Turkey Sandwich", "Turkey, avocado, mayo, cheese, lettuce", 390),
+      new Meal("Pad Thai", "Tasty, tasty Thai food", 750)
+    ];
+  }
 }
