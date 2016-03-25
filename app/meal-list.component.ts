5a48d0d260dd78b7ef73875849670dd2ebb04b11
Jordan Meier <jbirdmeier@gmail.com>
2016-03-25 10:23:08 -0700

    add a select menu to filter meals by calories, as well as import/add all relevant CaloriesPipe info to mealListComponent

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -3,13 +3,22 @@ import { Meal } from './meal.model';
 import { MealDetailsComponent } from './meal-details.component';
 import { EditMealComponent } from './edit-meal.component';
 import { NewMealComponent } from './new-meal.component';
+import { CaloriesPipe } from './calories.pipe';
 
 @Component  ({
   selector: 'meal-list',
   inputs: ['mealList'],
   directives: [MealDetailsComponent, NewMealComponent, EditMealComponent],
+  pipes: [CaloriesPipe],
   template: `
-    <div id="meal-list" *ngFor="#currentMeal of mealList">
+    <select (change)="onChangeCalories($event.target.value)" class="select">
+      <option value="all">Show All</option>
+      <option value="healthy">Show meals under 300 calories</option>
+      <option value="unhealthy">Show meals over 300 calories</option>
+    </select>
+
+
+    <div id="meal-list" *ngFor="#currentMeal of mealList | calories:filterCalories">
       <h3 (click)="mealWasSelected(currentMeal)">
         {{ currentMeal.name }}
       </h3>
@@ -22,8 +31,13 @@ import { NewMealComponent } from './new-meal.component';
 export class MealListComponent {
   public mealList: Meal[];
   public selectedMeal: Meal;
+  public filterCalories: string = "all";
 
   constructor () {}
+
+  onChangeCalories(filterOption) {
+    this.filterCalories = filterOption;
+  }
 
   mealWasSelected(clickedMeal: Meal): void {
     if(this.selectedMeal === clickedMeal) {
