d394630326b72e942f054019baf008082a5a7de0
Jordan Meier <jbirdmeier@gmail.com>
2016-03-25 11:12:58 -0700

    add breaks for styling in form

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -7,9 +7,9 @@ import { Meal } from './meal.model';
   template: `
     <div class="meal-form">
       <h3>Log a Meal:</h3>
-      <input placeholder="Meal Name" class='form-control input-lg' #newName>
-      <textarea placeholder="Meal Details" class="form-control" rows="3" #newDetails></textarea>
-      <input type="number" min="0" placeholder="Calories" class='form-control input-lg' #newCalories>
+      <input placeholder="Meal Name" class='form-control input-lg' #newName><br>
+      <textarea placeholder="Meal Details" class="form-control input-lg" rows="3" #newDetails></textarea><br>
+      <input type="number" min="0" placeholder="Calories" class='form-control input-lg' #newCalories><br>
       <button (click)="addMeal(newName, newDetails, newCalories)" class="btn-success btn-lg add-button">Add Meal</button>
     </div>
 
