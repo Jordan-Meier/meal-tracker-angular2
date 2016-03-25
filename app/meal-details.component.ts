544cdb6aa5912e56953d7fc54a1b35a8fd8829d7
Jordan Meier <jbirdmeier@gmail.com>
2016-03-25 09:22:35 -0700

    change the file and component name to better reflect what the component does

---
+++ app/meal-details.component.ts
@@ -0,0 +1,19 @@
+import { Component} from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component ({
+  selector: 'meal-details',
+  inputs: ['meal'],
+  template: `
+  <div class="meal-info">
+  <p>Meal: {{ meal.name }}</p>
+  <p>Details: $ {{ meal.details }}.00</p>
+  <p>Calories: {{ meal.calories }}</p>
+
+</div>
+  `
+})
+
+export class MealDetailsComponent {
+  public meal: Meal;
+}
