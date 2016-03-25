039e9b3ad7c0284ae0c6c8cf052aead7c16ef5bf
Jordan Meier <jbirdmeier@gmail.com>
2016-03-25 09:13:11 -0700

    add a method, mealWasSelected which will handle displaying meal details depending on whether or not a meal header is clicked

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -5,17 +5,30 @@ import { Meal } from './meal.model';
 import { NewMealComponent } from './new-meal.component';
 
 @Component  ({
-  selector: 'keg-list',
+  selector: 'meal-list',
   inputs: ['mealList'],
   directives: [NewMealComponent],
   template: `
+    <h3 (click)="mealWasSelected(currentMeal)">
+      {{ currentMeal.name }}
+    </h3>
+    <meal-details *ngIf="currentMeal === selectedMeal" [meal]="currentMeal"></meal-details>
     <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
   `
 })
 export class MealListComponent {
   public mealList: Meal[];
+  public selectedMeal: Meal;
 
   constructor () {}
+
+  mealWasSelected(clickedMeal: Meal): void {
+    if(this.selectedMeal === clickedMeal) {
+      this.selectedMeal = undefined;
+    } else {
+      this.selectedMeal = clickedMeal;
+    }
+  }
 
   createMeal(mealArray:any[]): void {
     var newMeal: Meal = new Meal(mealArray[0], mealArray[1], mealArray[2]);
